7ba44fad018fc9875dfb1d318e9d57df10ba1e1d
VinitKr <vinitkr28@gmail.com>
2024-11-13 19:47:32 +0530

    - udemy_flutter_dart_section1

--- udemy_flutter_and_dart_the_complete_guide/section1/lib/gradient_container.dart
+++ udemy_flutter_and_dart_the_complete_guide/section1/lib/gradient_container.dart
@@ -1,49 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:section1/dice_roller.dart';
 
 const startAlignmet = Alignment.topLeft;
 const endtAlignmet = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
+
+  //Need to remove const if, we created any variable like:
+  //var activeDiceImage = 'assets/images/dice-2.png';
+  //which is not const.
+
   const GradientContainer(this.color1, this.color2, {super.key});
 
   const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
 
+  /*GradientContainer(this.color1, this.color2, {super.key});
+
+  GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;*/
+
   final Color color1;
   final Color color2;
 
-  void rollDice() {}
+
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [color1, color2],
+          colors: [color1, color2, Colors.green],
           begin: startAlignmet,
           end: endtAlignmet,
         ),
       ),
       child: Center(
-        child: Column(mainAxisSize: MainAxisSize.min, children: [
-          Image.asset(
-            'assets/images/dice-2.png',
-            width: 200,
-          ),
-          // ElevatedButton(onPressed: rollDice, child: Text("data")),
-          // OutlinedButton(onPressed: (){}, child: Text("wwww")),
-
-          SizedBox(height: 20,),
-          TextButton(
-            onPressed: () {},
-            child: const Text("Roll Device"),
-            style: TextButton.styleFrom(
-              // padding: EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28)),
-          )
-        ]),
+        child: DiceRoller(),
       ),
     );
   }
