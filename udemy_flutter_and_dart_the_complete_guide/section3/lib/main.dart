9f5201e01209318ca6a0bcd36f14ae1ff3d8f3f1
VinitKr <vinitkr28@gmail.com>
2024-11-13 22:08:39 +0530

    folder correction

--- udemy_flutter_and_dart_the_complete_guide/section3/lib/main.dart
+++ udemy_flutter_and_dart_the_complete_guide/section3/lib/main.dart
@@ -3,13 +3,23 @@ import 'package:section1/gradient_container.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+      const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
           Color.fromARGB(255, 33, 5, 109),
           Color.fromARGB(255, 252, 12, 12),
         ),
       ),
-    ),
-  );
+    );
+  }
+
 }
